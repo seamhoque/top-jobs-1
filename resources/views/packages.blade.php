3da44919207ceff7ca62022f45cf243e5ceaf7e8
seam <seam.hoque@gmail.com>
2018-11-24 16:26:05 +0600

    packages updated

---
+++ resources/views/packages.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>job packages</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script type="text/javascript" src="script.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+<!-- This snippet uses Font Awesome 5 Free as a dependency. You can download it at fontawesome.io! -->
+
+<section class="pricing py-5">
+    <div class="container">
+        <div class="row">
+            <!-- Free Tier -->
+            <div class="col-lg-4">
+                <div class="card mb-5 mb-lg-0">
+                    <div class="card-body">
+                        <h5 class="card-title text-muted text-uppercase text-center">Basic Jobs</h5>
+                        <h6 class="card-price text-center">$10.00<span class="period"></span></h6>
+                        <hr>
+                        <ul class="fa-ul">
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>1 job posting</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>0 featured job</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Job displayed for 20 days</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Premium Support 24/7</li>
+                        </ul>
+                        <a href="#" class="btn btn-block btn-primary text-uppercase">ADD TO CART</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Plus Tier -->
+            <div class="col-lg-4">
+                <div class="card mb-5 mb-lg-0">
+                    <div class="card-body">
+                        <h5 class="card-title text-muted text-uppercase text-center">Standard Jobs</h5>
+                        <h6 class="card-price text-center">$45.00<span class="period"></span></h6>
+                        <hr>
+                        <ul class="fa-ul">
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>45 job posting</strong></li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>30 featured job</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Job displayed for 45 days</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Premium Support 24/7</li>
+                        </ul>
+                        <a href="#" class="btn btn-block btn-primary text-uppercase">ADD TO CART</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Pro Tier -->
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title text-muted text-uppercase text-center">Golden Jobs</h5>
+                        <h6 class="card-price text-center">$80.00<span class="period"></span></h6>
+                        <hr>
+                        <ul class="fa-ul">
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span><strong>90 job posting</strong></li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span> 35 featured job</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Job displayed for 60 days</li>
+                            <li><span class="fa-li"><i class="fas fa-check"></i></span>Premium Support 24/7</li>
+                        </ul>
+                        <a href="#" class="btn btn-block btn-primary text-uppercase">ADD TO CART</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
