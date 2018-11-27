dedff885252add43cd14e200a5884e654880de3f
Arhan Islam <arhanislam3407@gmail.com>
2018-11-27 22:15:09 +0600

    Nav bar added

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -74,11 +74,74 @@
 
     <!-- Core JS files -->
     <script src="{{ asset('js/app.js') }}"></script>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="js/main.js"></script>
+
     <!-- /core JS files -->
 
     <!-- Plugin JS Files -->
     @stack('scripts-plugin')
+    <script>
+        var TxtRotate = function(el, toRotate, period) {
+            this.toRotate = toRotate;
+            this.el = el;
+            this.loopNum = 0;
+            this.period = parseInt(period, 10) || 2000;
+            this.txt = '';
+            this.tick();
+            this.isDeleting = false;
+        };
 
+        TxtRotate.prototype.tick = function() {
+            var i = this.loopNum % this.toRotate.length;
+            var fullTxt = this.toRotate[i];
+
+            if (this.isDeleting) {
+                this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+                this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
+
+            this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+            var that = this;
+            var delta = 300 - Math.random() * 100;
+
+            if (this.isDeleting) { delta /= 2; }
+
+            if (!this.isDeleting && this.txt === fullTxt) {
+                delta = this.period;
+                this.isDeleting = true;
+            } else if (this.isDeleting && this.txt === '') {
+                this.isDeleting = false;
+                this.loopNum++;
+                delta = 500;
+            }
+
+            setTimeout(function() {
+                that.tick();
+            }, delta);
+        };
+
+        window.onload = function() {
+            var elements = document.getElementsByClassName('txt-rotate');
+            for (var i=0; i<elements.length; i++) {
+                var toRotate = elements[i].getAttribute('data-rotate');
+                var period = elements[i].getAttribute('data-period');
+                if (toRotate) {
+                    new TxtRotate(elements[i], JSON.parse(toRotate), period);
+                }
+            }
+            // INJECT CSS
+            var css = document.createElement("style");
+            css.type = "text/css";
+            css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+            document.body.appendChild(css);
+        };
+    </script>
     <!-- Page Scripts -->
     @stack('scripts')
 
--- resources/views/welcome/components/section-home.blade.php
+++ resources/views/welcome/components/section-home.blade.php
@@ -1,3 +1,57 @@
-<div class="section-home">
-    <h1>Section Home</h1>
+<!---Menu and Banner Section Start--->
+<div class="banner">
+    <div class="container-fluid">
+        <div class="row">
+            <!--Logo Rotating Start-->
+            <div class="col-lg-2">
+                <img src="img/logoo.png" alt="">
+            </div>
+            <!--Logo Rotating End-->
+        </div>
+        <div class="row">
+            <!--Search Form Section Start-->
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8">
+                <div class="search-job2">
+                    <form>
+                        <div class="row no-gape">
+                            <div class="col-lg-4 col-md-3 col-sm-4">
+                                <div class="job-field">
+                                    <input type="text" placeholder="Keywords"/>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-4">
+                                <div class="job-field">
+                                    <select id="">
+                                        <option value="hide">--All Regions--</option>
+                                        <option value="Istanbul" rel="icon-temperature">Istanbul</option>
+                                        <option value="New York">New York</option>
+                                        <option value="London">London</option>
+                                        <option value="Russia">Russia</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-4">
+                                <div class="job-field">
+                                    <select class="">
+                                        <option value="hide">--All Category--</option>
+                                        <option value="Mecahnic">Mechanic</option>
+                                        <option value="web development">Web Development</option>
+                                        <option value="car install">Car Install</option>
+                                        <option value="shoes slippers">Shoes Slippers</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-lg-2  col-md-3 col-sm-12">
+                                <button class="banner_button" type="submit"> <i class="fab fa-searchengin"></i> FIND JOB <i class="la la-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!--Search Form Section End-->
+        </div>
+    </div>
 </div>
+<!---Menu and Banner Section End--->
