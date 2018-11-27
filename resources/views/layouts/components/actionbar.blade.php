94915de079f9c92b3a4c11f828a96cc59f4a6156
Arhan Islam <arhanislam3407@gmail.com>
2018-11-27 18:46:41 +0600

    Nav bar added

--- resources/views/layouts/components/actionbar.blade.php
+++ resources/views/layouts/components/actionbar.blade.php
@@ -2,16 +2,14 @@
 <div class="action_bar">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 nopad" id="action_bar_left"> <!--Action Bar left Section Start-->
-                <div class="action_bar_left_main center-block">
-                <h4 style="color:white;">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 center-block nopad" id="action_bar_left"> <!--Action Bar left Section Start-->
+                <h4 class="center-block" style="color:white;">
                     <i style="font-size:16px" class="fas fa-map-marked-alt"></i>&nbsp;<strong>Address:</strong> CodersTrust Bangladesh,Bonani-11,Dhaka-1215
                 </h4>
-                </div>
             </div> <!--Action Bar left Section End-->
 
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 nopad" id="action_bar_right"><!--Action Bar Right Section Start-->
-                <div class="social_icon">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 center-block nopad" id="action_bar_right"><!--Action Bar Right Section Start-->
+                <div class="social_icon center-block">
                     <i class="fas fa-envelope"></i>
                     <i class="fab fa-linkedin-in"></i>
                     <i class="fab fa-facebook"></i>
